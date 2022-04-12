9ef19956e1f5a29f3f5e83f20d1a3198c7f9551d
AidanHimley <ah8uqq@virginia.edu>
2022-04-12 12:53:11 -0400

    Initial commit. Skeleton based on wall following code

---
+++ src/control.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import rospy
+from gap.msg import gap_info
+from ackermann_msgs.msg import AckermannDrive
+from collections import deque
+
+class Controller():
+
+	def __init__(self, servo_offset=0.0):
+		# initialize node
+		rospy.init_node('car7/controller', anonymous=True)
+
+		# This code can input desired velocity from the user.
+		# velocity must be between [0,100] to move forward. 
+		# The following velocity values correspond to different speed profiles.
+		# 15: Very Slow (Good for debug mode)
+		# 25: Slow and steady
+		# 35: Nice Autonomous Pace
+		# > 40: Careful, what you do here. Only use this if your autonomous steering is very reliable.
+		# this is used as max velocity for dynamic velocity
+		self.max_velocity = float(input("Enter desired maximum velocity: "))
+		self.min_velocity = 15
+
+		# zero correction offset in case servo is misaligned and has a bias in turning.
+		self.servo_offset = servo_offset
+
+		# initialize subscriber and publisher (do this after parameters have been set)
+		rospy.Subscriber('car7/gap_info', gap_info, self.control)
+		self.command_pub = rospy.Publisher('/car_7/offboard/command', AckermannDrive, queue_size = 1)
+
+		rospy.spin()
+
+	def control(self, data):
+		angle = 0
+
+		#----------------control logic goes here-------------------
+
+		# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
+		command = AckermannDrive()
+
+		# Make sure the steering value is within bounds [-100,100]
+		command.steering_angle = max(-100, min(angle, 100))
+
+		# Make sure the velocity is within bounds [0,100]
+		velocity = ((100 - abs(command.steering_angle))/100)**2 * ((self.max_velocity - self.min_velocity)) + self.min_velocity
+		command.speed = max(0, min(velocity, 100))
+
+		# Move the car autonomously
+		self.command_pub.publish(command)
+
+if __name__ == '__main__':
+	Controller()
---
+++ src/gap_finder.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import rospy
+import math
+from sensor_msgs.msg import LaserScan
+from gap.msg import gap_info
+
+class GapFinder():
+
+	def __init__(self):
+		print("Hokuyo LIDAR node started")
+		
+		# initialize node, subscriber, and publisher
+		rospy.init_node('car7/gap_finder', anonymous = True)
+		rospy.Subscriber("/car_7/scan", LaserScan, self.callback)
+		self.pub = rospy.Publisher('car7/error', gap_info, queue_size=10)
+
+		# Some useful variable declarations.
+		self.ANGLE_RANGE = 240			# Hokuyo 4LX has 240 degrees FoV for scan
+		self.CAR_LENGTH = 0.50			# Traxxas Rally is 20 inches or 0.5 meters
+		
+		rospy.spin()
+
+	def getRange(self, data, angle):
+		"""data: single message from topic /scan
+		angle: between -30 to 210 degrees, where 0 degrees is directly to the right, and 90 degrees is directly in front
+		Outputs length in meters to object with angle in lidar scan field of view\n
+		Cleans NaNs etc"""
+		if (angle < -30 or angle > 210):
+			return data.range_max
+		
+		angle = math.radians(angle)
+		
+		index = int((angle+math.radians(30)) / data.angle_increment)
+		range = data.ranges[index]
+
+		if (range and range < data.range_max):
+			return range
+		return data.range_max
+
+	def callback(self, data):
+
+		#-------------------gap-finding logic goes here------------------------
+
+		msg = gap_info()		# An empty msg is created of the type gap_info
+		msg.angle = ...			# position of the center of the selected gap
+		msg.width = ...			# width of the selected gap
+		msg.depth = ...			# depth of the selected gap
+		self.pub.publish(msg)
+
+if __name__ == '__main__':
+	GapFinder()
